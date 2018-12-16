d6cd6661a16545de9da828ccce3162b1eb2d548f
Alex Seppala <aseppala@alexs-mbp.lan>
2018-12-16 13:46:02 -0800

    could use more commenting, but preprocessor is ready for batch downsampling

---
+++ midi_initial_preprocessing/downsample_midis.py
@@ -0,0 +1,53 @@
+from mido import MidiFile, MidiTrack, Message, merge_tracks
+from music21 import *
+import os
+
+directory = "midi_initial_preprocessing/src_midis"
+
+def downsample(filename):
+    filepath = 'midi_initial_preprocessing/src_midis/' + filename
+
+    parsed_midi = midi.MidiFile()
+    parsed_midi.open(filepath, "rb")
+    parsed_midi.read()
+    parsed_midi.close()
+    parsed_midi_tracks = parsed_midi.tracks
+    #get channels for each track to find where the percussion track is (channel 10 music21 channel 9 mido)
+    percussion_track_indexes = []
+    for track in range(len(parsed_midi_tracks)):
+        channels = parsed_midi_tracks[track].getChannels()
+        if(10 in channels):
+            percussion_track_indexes.append(track)
+
+    mid = MidiFile(filepath)
+
+    other_tracks = []
+    percussion_tracks = []
+
+    for track in range(len(mid.tracks)):
+        if track in percussion_track_indexes:
+            percussion_tracks.append(mid.tracks[track])
+        else:
+            other_tracks.append(mid.tracks[track])
+        
+    percussion_track = merge_tracks(percussion_tracks)
+    other_track = merge_tracks(other_tracks)
+
+    percussion_track = MidiTrack([msg for msg in percussion_track if msg.type == "note_on" or msg.type == "note_off"])
+    other_track = MidiTrack([msg for msg in other_track if msg.type == "note_on" or msg.type == "note_off"])
+        
+    out_mid = MidiFile()
+    out_mid.tracks = [other_track,percussion_track]
+
+    #output to temp midi file
+    out_mid.save('midi_initial_preprocessing/dst_midis/' + filename)
+
+for filename in os.listdir(directory):
+    if filename.endswith(".mid"):
+        downsample(filename)
+        continue
+    else:
+        continue
+
+
+
