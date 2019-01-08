de578b61c7ae018eed29677fb4d60edbaec4d82e
Alex Seppala <aseppala@Alexs-MacBook-Pro.local>
2019-01-08 16:58:24 -0500

    wip but mostly working lmao

--- make_notes_dictionary_step_2/make_notes_dictionary.py
+++ make_notes_dictionary_step_2/make_notes_dictionary.py
@@ -1,12 +1,15 @@
 from music21 import *
 import os
 import numpy as np
+import pickle
 
-directory = "midi_downsampling/dst_midis"
+directory = "midi_downsampling_step_1/dst_midis"
+
+output_dictionary = []
 
 for filename in os.listdir(directory):
     if filename.endswith(".mid"):
-        parsed_midi = converter.parse('midi_downsampling/dst_midis/' + filename)
+        parsed_midi = converter.parse('midi_downsampling_step_1/dst_midis/' + filename)
 
         #flat combines all voices(not sure why it wasnt fully flattened before, but had to move on)
         #chordify combines all notes with same offset into a chord for each stream
@@ -32,13 +35,31 @@ for filename in os.listdir(directory):
             note_array_piano[round(piano_stream[i].offset/smallest_interval)] = piano_stream[i]
         for i in range(len(percussion_stream)):
             note_array_percussion[round(percussion_stream[i].offset/smallest_interval)] = percussion_stream[i]
-        
-        #printing for testing
-        #they seem to sync up!
-        # for i in range(len(note_array_percussion)):
-        #     print(str(note_array_piano[i]) + "|" + str(note_array_percussion[i]) + "\n")
 
-        #insert normalizing code here using the notes and found in the chosen downsampled dataset
+        for i in range(len(note_array_percussion)):
+            piano_pitches = ""
+            percussion_pitches = ""
+            if note_array_piano[i] != 0:
+                if not note_array_piano[i].isRest:
+                #     note_array_piano[i] = 0
+                # else:
+                    for pitch in range(len(note_array_piano[i].pitches)):
+                        piano_pitches += note_array_piano[i].pitches[pitch].nameWithOctave
+                        piano_pitches += " "
+            if note_array_percussion[i] != 0:
+                if not note_array_percussion[i].isRest:
+                #     note_array_percussion[i] = 0
+                # else:
+                    for pitch in range(len(note_array_percussion[i].pitches)):
+                        percussion_pitches += note_array_percussion[i].pitches[pitch].nameWithOctave
+                        percussion_pitches += " "
+            if (piano_pitches,percussion_pitches) not in output_dictionary:
+                output_dictionary.append((piano_pitches,percussion_pitches))
+
+        #insert normalizing code here using the notes found in the chosen downsampled dataset
         continue
     else:
         continue
+
+with open("make_notes_dictionary_step_2/notes_dictionary.txt", "wb") as fp:   #Pickling
+    pickle.dump(output_dictionary, fp)
